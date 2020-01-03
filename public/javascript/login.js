23dc69ab175ddf849b37d83584669b2fa9873805
idjaeha <jaehanim1@gmail.com>
2020-01-04 02:45:43 +0900

    [add] button css

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -10,139 +10,137 @@ const memoTitle = memoDiv.querySelector(".js-memoTitle");
 const ACCOUNTS_LS = "accounts";
 const AUTOLOGIN_LS = "autoLogin";
 let accounts = [];
-let currentAccountObj = {
-
-};
+let currentAccountObj = {};
 
 function createAccount(id, pwd) {
-    // 새로운 계정을 생성하는 함수
-    // arg 1 : id ( 사용할 아이디 )
-    // arg 2 : pwd ( 사용할 비밀번호 )
-    const createdDate = new Date().toJSON();
-    accountObj = {
-        id,
-        password : pwd,
-        createdDate
-    };
-    accounts.push(accountObj);
-    saveAccounts();
+  // 새로운 계정을 생성하는 함수
+  // arg 1 : id ( 사용할 아이디 )
+  // arg 2 : pwd ( 사용할 비밀번호 )
+  const createdDate = new Date().toJSON();
+  accountObj = {
+    id,
+    password: pwd,
+    createdDate
+  };
+  accounts.push(accountObj);
+  saveAccounts();
 }
 
 function loginAccount(id, pwd) {
-    // 로그인 하는 함수
-    // arg 1 : id ( 사용할 아이디 )
-    // arg 2 : pwd ( 사용할 비밀번호 )
-    idInput.value = ""
-    passwordInput.value = ""
+  // 로그인 하는 함수
+  // arg 1 : id ( 사용할 아이디 )
+  // arg 2 : pwd ( 사용할 비밀번호 )
+  idInput.value = "";
+  passwordInput.value = "";
 
-    loginDiv.classList.remove("showing");
-    memoDiv.classList.add("showing");
-    memoTitle.innerText = `${id}'s Small Memo`;
-    const loginObj = {
-        id,
-        password: pwd
-    }
+  loginDiv.classList.remove("showing");
+  memoDiv.classList.add("showing");
+  memoTitle.innerText = `${id}'s Small Memo`;
+  const loginObj = {
+    id,
+    password: pwd
+  };
 
-    currentAccountObj = loginObj;
-    localStorage.setItem(AUTOLOGIN_LS, JSON.stringify(loginObj));
+  currentAccountObj = loginObj;
+  localStorage.setItem(AUTOLOGIN_LS, JSON.stringify(loginObj));
 }
 
 function checkAccount(id, pwd) {
-    // 계정이 유효한지 판단하는 함수
-    // 0 : 성공, 존재하는 계정
-    // 1 : 성공, 존재하지 않는 계정
-    // 2 : 실패, id와 pwd 불일치
-    // 3 : 실패, id 혹은 pwd이 빈칸으로 존재
-    if (id === "" || pwd === "") {
-        return 3;
+  // 계정이 유효한지 판단하는 함수
+  // 0 : 성공, 존재하는 계정
+  // 1 : 성공, 존재하지 않는 계정
+  // 2 : 실패, id와 pwd 불일치
+  // 3 : 실패, id 혹은 pwd이 빈칸으로 존재
+  if (id === "" || pwd === "") {
+    return 3;
+  }
+
+  let flag = 1;
+
+  accounts.some(function(account) {
+    if (account.id === id && account.password === pwd) {
+      flag = 0;
+      return true;
+    } else if (account.id === id && account.password !== pwd) {
+      flag = 2;
+      return true;
+    } else {
+      return false;
     }
+  });
 
-    let flag = 1;
-
-    accounts.some(function(account) {
-        if (account.id === id && account.password === pwd) {
-            flag = 0;
-            return true;
-        } else if ( account.id === id && account.password !== pwd) {
-            flag = 2;
-            return true;
-        } else {
-            return false;
-        }
-    });
-
-    return flag;
+  return flag;
 }
 
 function saveAccounts() {
-    // 계정 정보를 저장하는 함수
-    localStorage.setItem(ACCOUNTS_LS, JSON.stringify(accounts));
+  // 계정 정보를 저장하는 함수
+  localStorage.setItem(ACCOUNTS_LS, JSON.stringify(accounts));
 }
 
 function loadAccounts() {
-    // 기존의 계정 정보를 불러와 accounts에 저장하는 함수
-    const loadedAccounts = localStorage.getItem(ACCOUNTS_LS);
-    if (loadedAccounts === null) {
-        saveAccounts();
-    } else {
-        accounts = JSON.parse(loadedAccounts);
-    }
+  // 기존의 계정 정보를 불러와 accounts에 저장하는 함수
+  const loadedAccounts = localStorage.getItem(ACCOUNTS_LS);
+  if (loadedAccounts === null) {
+    saveAccounts();
+  } else {
+    accounts = JSON.parse(loadedAccounts);
+  }
 }
 
 function handleLogin(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        // loginForm 에서 submit 이 발생했을 때 사용되는 핸들
-        const enteredId = idInput.value.trim();
-        const enteredPwd = passwordInput.value.trim();
-        loadAccounts();
-        const check = checkAccount(enteredId, enteredPwd);
-        if (check === 0) {
-            // 성공, 존재하는 계정
-            loginAccount(enteredId, enteredPwd);
-            msg.innerText = "";
-        } else if(check === 1) {
-            // 성공, 존재하지 않는 계정
-            createAccount(enteredId, enteredPwd);
-            loginAccount(enteredId, enteredPwd);
-            msg.innerText = "";
-        } else if(check === 2) {
-            // 실패, id와 pwd 불일치
-            msg.innerText = "올바른 id와 pwd를 입력해주세요.";
-        } else if(check === 3) {
-            // 실패, id 혹은 pwd 빈칸으로 존재
-            msg.innerText = "id 혹은 pwd를 입력해주세요.";
-        }
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    // loginForm 에서 submit 이 발생했을 때 사용되는 핸들
+    const enteredId = idInput.value.trim();
+    const enteredPwd = passwordInput.value.trim();
+    loadAccounts();
+    const check = checkAccount(enteredId, enteredPwd);
+    if (check === 0) {
+      // 성공, 존재하는 계정
+      loginAccount(enteredId, enteredPwd);
+      msg.innerText = "";
+    } else if (check === 1) {
+      // 성공, 존재하지 않는 계정
+      createAccount(enteredId, enteredPwd);
+      loginAccount(enteredId, enteredPwd);
+      msg.innerText = "";
+    } else if (check === 2) {
+      // 실패, id와 pwd 불일치
+      msg.innerText = "올바른 id와 pwd를 입력해주세요.";
+    } else if (check === 3) {
+      // 실패, id 혹은 pwd 빈칸으로 존재
+      msg.innerText = "id 혹은 pwd를 입력해주세요.";
     }
+  }
 }
 
 function autoLogin() {
-    // 자동 로그인 여부에 대해 판단하는 함수
-    const loadedAutoLogin = localStorage.getItem(AUTOLOGIN_LS);
-    loadAccounts();
-    if (loadedAutoLogin !== null) {
-        const parsedAccount = JSON.parse(loadedAutoLogin)
-        if (checkAccount(parsedAccount.id, parsedAccount.password) === 0) {
-            loginAccount(parsedAccount.id, parsedAccount.password);
-        }
+  // 자동 로그인 여부에 대해 판단하는 함수
+  const loadedAutoLogin = localStorage.getItem(AUTOLOGIN_LS);
+  loadAccounts();
+  if (loadedAutoLogin !== null) {
+    const parsedAccount = JSON.parse(loadedAutoLogin);
+    if (checkAccount(parsedAccount.id, parsedAccount.password) === 0) {
+      loginAccount(parsedAccount.id, parsedAccount.password);
     }
+  }
 }
 
 function handleLogout(event) {
-    // 로그아웃 버튼을 누르게 되면 발생하는 이벤트를 관리하는 핸들
-    event.preventDefault();
-    currentAccountObj = {};
-    localStorage.removeItem(AUTOLOGIN_LS);
+  // 로그아웃 버튼을 누르게 되면 발생하는 이벤트를 관리하는 핸들
+  event.preventDefault();
+  currentAccountObj = {};
+  localStorage.removeItem(AUTOLOGIN_LS);
 
-    loginDiv.classList.add("showing");
-    memoDiv.classList.remove("showing");
+  loginDiv.classList.add("showing");
+  memoDiv.classList.remove("showing");
 }
 
 function init() {
-    autoLogin();
-    idInput.addEventListener("keyup", handleLogin);
-    passwordInput.addEventListener("keyup", handleLogin);
-    logoutBtn.addEventListener("click", handleLogout);
+  autoLogin();
+  idInput.addEventListener("keyup", handleLogin);
+  passwordInput.addEventListener("keyup", handleLogin);
+  logoutBtn.addEventListener("click", handleLogout);
 }
 
 init();
--- public/javascript/memo.js
+++ public/javascript/memo.js
@@ -3,135 +3,135 @@ const memoTextarea = memoForm.querySelector("textarea");
 const memoInput = memoForm.querySelector("input");
 const memoTable = document.querySelector(".js-memoTable");
 
-const MEMOS_LS = 'memos';
+const MEMOS_LS = "memos";
 
 let memos = [];
 
 function deleteMemo(event) {
-    event.preventDefault();
-    const btn = event.target;
-    const td = btn.parentNode;
-    memoTable.removeChild(td);
-    const cleanMemos = memos.filter(function(memo) {
-        return memo.id !== td.id;
-    });
-    memos = cleanMemos;
-    saveMemos();
+  event.preventDefault();
+  const btn = event.target;
+  const td = btn.parentNode;
+  memoTable.removeChild(td);
+  const cleanMemos = memos.filter(function(memo) {
+    return memo.id !== td.id;
+  });
+  memos = cleanMemos;
+  saveMemos();
 }
 
 function updateMemo(event) {
-    event.preventDefault();
-    const btn = event.target;
-    const currentContent = btn.parentNode.querySelector("textarea").value;
-    const currentTitle = btn.parentNode.querySelector("input").value;
-    const currentDate = btn.parentNode.querySelector("h5");
-    const currentId = btn.parentNode.id;
-    
+  event.preventDefault();
+  const btn = event.target;
+  const currentContent = btn.parentNode.querySelector("textarea").value;
+  const currentTitle = btn.parentNode.querySelector("input").value;
+  const currentDate = btn.parentNode.querySelector("h5");
+  const currentId = btn.parentNode.id;
 
-    const loadedMemos = localStorage.getItem(MEMOS_LS);
-    const parsedMemos = JSON.parse(loadedMemos);
-    const now = new Date();
-    currentDate.innerHTML = getTime(now);
+  const loadedMemos = localStorage.getItem(MEMOS_LS);
+  const parsedMemos = JSON.parse(loadedMemos);
+  const now = new Date();
+  currentDate.innerHTML = getTime(now);
 
-    parsedMemos.forEach(function(memo) {
-        if(memo.id === currentId) {
-            memo.title = currentTitle;
-            memo.content = currentContent;
-            memo.date = now;
-        }
-    });
+  parsedMemos.forEach(function(memo) {
+    if (memo.id === currentId) {
+      memo.title = currentTitle;
+      memo.content = currentContent;
+      memo.date = now;
+    }
+  });
 
-    memos = parsedMemos;
-    saveMemos();
+  memos = parsedMemos;
+  saveMemos();
 }
 
 function saveMemos() {
-    localStorage.setItem(MEMOS_LS, JSON.stringify(memos));
+  localStorage.setItem(MEMOS_LS, JSON.stringify(memos));
 }
 
 function paintMemo(title, content, date = null, id = null) {
-    const newId = id === null ? new Date().toJSON() : id; //해당 옵션을 주지 않는 경우 태그와 로컬 스토리지의 값이 다르게 표현된다. toJSON은 문자열로 변경하는 것과 의미가 같다.
-    const newDate = date === null ? newId : date;
-    const td = getMemoObj(title, content, newDate, newId);
-    memoTable.appendChild(td);
-    const memoObj = {
-        id : newId,
-        title : title,
-        content : content,
-        date : newDate
-    };
-    memos.push(memoObj);
+  const newId = id === null ? new Date().toJSON() : id; //해당 옵션을 주지 않는 경우 태그와 로컬 스토리지의 값이 다르게 표현된다. toJSON은 문자열로 변경하는 것과 의미가 같다.
+  const newDate = date === null ? newId : date;
+  const td = getMemoObj(title, content, newDate, newId);
+  memoTable.appendChild(td);
+  const memoObj = {
+    id: newId,
+    title: title,
+    content: content,
+    date: newDate
+  };
+  memos.push(memoObj);
 }
 
 function getMemoObj(title, content, date, id) {
-    _div = document.createElement("div");
-    _title = document.createElement("input");
-    _date = document.createElement("h5");
-    _content = document.createElement("textarea");
-    _br = document.createElement("br");
-    _delBtn = document.createElement("button");
-    _updateBtn = document.createElement("button");
-    
-    _title.value = title;
-    _content.value = content;
-    _date.innerHTML = getTime(date);
-    _delBtn.innerHTML = "삭제";
-    _updateBtn.innerHTML = "수정";
-    _delBtn.addEventListener("click", deleteMemo);
-    _updateBtn.addEventListener("click", updateMemo);
-    _div.id = id;
-    _div.classList.add("memoObj");
+  _div = document.createElement("div");
+  _title = document.createElement("input");
+  _date = document.createElement("h5");
+  _content = document.createElement("textarea");
+  _br = document.createElement("br");
+  _delBtn = document.createElement("button");
+  _updateBtn = document.createElement("button");
 
-    _div.appendChild(_title);
-    _div.appendChild(_updateBtn);
-    _div.appendChild(_delBtn);
-    _div.appendChild(_br);
-    _div.appendChild(_content);
-    _div.appendChild(_date);
-    
-    return _div;
+  _title.value = title;
+  _content.value = content;
+  _date.innerHTML = getTime(date);
+  _delBtn.innerHTML = "삭제";
+  _updateBtn.innerHTML = "수정";
+  _delBtn.addEventListener("click", deleteMemo);
+  _updateBtn.addEventListener("click", updateMemo);
+  _div.id = id;
+  _div.classList.add("memoObj");
+  _title.classList.add("memoObjTitle");
+  _content.classList.add("memoObjContent");
+
+  _div.appendChild(_title);
+  _div.appendChild(_updateBtn);
+  _div.appendChild(_delBtn);
+  _div.appendChild(_br);
+  _div.appendChild(_content);
+  _div.appendChild(_date);
+
+  return _div;
 }
 
 function getTime(date = null) {
-    const curDate = date === null ? new Date() : new Date(date);
-    const hours = curDate.getHours(), 
-        minutes = curDate.getMinutes(),
-        seconds = curDate.getSeconds(),
-        year = curDate.getFullYear(),
-        mon = curDate.getMonth() + 1,
-        day = curDate.getDate();
+  const curDate = date === null ? new Date() : new Date(date);
+  const hours = curDate.getHours(),
+    minutes = curDate.getMinutes(),
+    seconds = curDate.getSeconds(),
+    year = curDate.getFullYear(),
+    mon = curDate.getMonth() + 1,
+    day = curDate.getDate();
 
-    const currentTime = `
+  const currentTime = `
     ${year}년 ${mon}월 ${day}일
     ${hours < 10 ? `0${hours}` : hours}:${
-        minutes < 10 ? `0${minutes}` : minutes}:${
-            seconds < 10 ? `0${seconds}` : seconds}`;
-    return currentTime;
-    
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  return currentTime;
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const currentTitle = memoInput.value;
-    const currentContent = memoTextarea.value;
-    paintMemo(currentTitle, currentContent);
-    memoInput.value = "";
-    memoTextarea.value= "";
-    saveMemos();
+  event.preventDefault();
+  const currentTitle = memoInput.value;
+  const currentContent = memoTextarea.value;
+  paintMemo(currentTitle, currentContent);
+  memoInput.value = "";
+  memoTextarea.value = "";
+  saveMemos();
 }
 function loadMemos() {
-    const loadedMemos = localStorage.getItem(MEMOS_LS);
-    if(loadedMemos !== null) {
-        const parsedMemos = JSON.parse(loadedMemos);
-        parsedMemos.forEach(function(memo) {
-            paintMemo(memo.title, memo.content, memo.date, memo.id);
-        });
-    }
+  const loadedMemos = localStorage.getItem(MEMOS_LS);
+  if (loadedMemos !== null) {
+    const parsedMemos = JSON.parse(loadedMemos);
+    parsedMemos.forEach(function(memo) {
+      paintMemo(memo.title, memo.content, memo.date, memo.id);
+    });
+  }
 }
 
 function init() {
-    loadMemos();
-    memoForm.addEventListener("submit", handleSubmit);
+  loadMemos();
+  memoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
